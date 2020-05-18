061b2effb927e906d528c0ce8044d9aecd1aedd6
Ryan McCahon <mccahonr@yahoo.com>
2020-05-18 23:32:54 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,4 +2,13 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 def Dancer
+  
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+  
 end
